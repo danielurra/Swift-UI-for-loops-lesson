47c094666409b2bc136a7eaa4802b396a66b0973
Daniel Urra <danielurra75@gmail.com>
2024-03-24 06:00:15 -0500

    Create for-loops-lesson.swift

---
+++ for-loops-lesson.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var favFruits: [String] = ["banana", "mango", "papaya", "ciruela",]
+
+
+for i in 1...5 {
+    
+    print(i)
+    
+}
+
+
+for fruitsForLoop in favFruits {
+    
+    print(fruitsForLoop)
+    
+}
+ 
+for numberInt in 1...3 {
+    
+    print(numberInt * 3)
+}
+
+for numberDouble in 1...3 {
+    
+    print(Double(numberDouble) / 3.0)
+    
+}
